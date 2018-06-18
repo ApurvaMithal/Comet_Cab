4cb734fcbb0fab181b2c13e544084a04677720bc
ApurvaMithal <mithal.apurva29@gmail.com>
2018-06-18 02:04:21 -0500

    Second Commit

--- Comet_Cab/src/model/Cab.java
+++ Comet_Cab/src/model/Cab.java
@@ -1,28 +1,16 @@
 package model;
 
 public class Cab {
-	private String vehicleType;
 	private String licensePlateNo;
-	private float ratePerMile;
 	private String model;
 	private CabType cabType;
 	private boolean availabilty;
 
-	public Cab(String vehicleType, String licensePlateNo, float ratePerMile, String model, CabType cabType) {
+	public Cab(String licensePlateNo, String model, CabType cabType) {
 		super();
-		this.vehicleType = vehicleType;
 		this.licensePlateNo = licensePlateNo;
-		this.ratePerMile = ratePerMile;
 		this.model = model;
 		this.cabType = cabType;
-	}
-
-	public String getVehicleType() {
-		return vehicleType;
-	}
-
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
 	}
 
 	public String getLicensePlateNo() {
@@ -31,14 +19,6 @@ public class Cab {
 
 	public void setLicensePlateNo(String licensePlateNo) {
 		this.licensePlateNo = licensePlateNo;
-	}
-
-	public float getRatePerMile() {
-		return ratePerMile;
-	}
-
-	public void setRatePerMile(float ratePerMile) {
-		this.ratePerMile = ratePerMile;
 	}
 
 	public String getModel() {
